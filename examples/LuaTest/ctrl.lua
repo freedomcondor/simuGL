d07d91ff7dc3e3fe5b13309233d7b1c923e3e8fa
Condor3397 <freedomcondor@126.com>
2020-01-28 14:41:51 +0100

    add a arrangemnet lua tool

--- examples/LuaTest/ctrl.lua
+++ examples/LuaTest/ctrl.lua
@@ -2,9 +2,14 @@ package.path = package.path .. ";" .. SIMU_LUA_DIR .. "/?.lua"
 
 Vec3 = require("Vector3")
 Qua = require("Quaternion")
+Arrangement = require("Arrangement")
+require("Debugger")
 
+local arranger
 function init()
 	print("I am init")
+	print(SIMU_LUA_DIR)
+	print("--- vector quaternion test ---")
 	local q1 = Qua:createFrom4Vector3s(Vec3(1,0,0), 
 	                                   Vec3(0,1,0),
 	                                   Vec3(1,1,0),
@@ -13,13 +18,22 @@ function init()
 	print(q1:getAxis())
 	print(q1:getAng())
 	print(q1:toRotate(Vec3(0,0,1)))
+
+	print("--- arranger test ---")
+	arranger = Arrangement:newArranger{1, "aaa", {"nihao"}, 4, 5}
+	local aa = arranger()
+	while aa ~= nil do
+		for i = 1, #aa do print(i, aa[i]) end
+		print("---")
+		aa = arranger()
+	end
 end
 
 function step()
-	print(SIMU_LUA_DIR)
+	print("I am step")
+	print("--- vector quaternion test ---")
 	print(Vec3(0,0,0))
 	print(Qua(0,0,1,math.pi):toRotate(Vec3(0,0,0)))
-	print("I am step")
 end
 
 function exit()
